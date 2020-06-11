adf4acceb056d82898a1583645b44cfe99528b55
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-06-11 11:29:45 +0900

    🛠create project

---
+++ TeePod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeePod
+//
+//  Created by 吉川勇太郎 on 2020/06/11.
+//  Copyright © 2020 TeePod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
