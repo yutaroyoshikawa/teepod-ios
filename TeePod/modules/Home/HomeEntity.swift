5ea6277ecfb8205e1115a3ce87df624206031754
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-06-20 15:47:18 +0900

    entityの形に修正

--- TeePod/modules/Home/HomeEntity.swift
+++ TeePod/modules/Home/HomeEntity.swift
@@ -10,10 +10,3 @@ import Foundation
 import SwiftUI
 
 struct HomeEntiry {}
-
-
-struct HomeEntity_Previews: PreviewProvider {
-  static var previews: some View {
-    /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-  }
-}
