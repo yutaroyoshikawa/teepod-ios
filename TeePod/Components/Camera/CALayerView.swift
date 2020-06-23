88d295e56eb65f153c5ecc4008f25d4c67c9eed1
ChiekoIshii <chi24601@gmail.com>
2020-06-23 22:19:43 +0900

    カメラ導入🐸

---
+++ TeePod/Components/Camera/CALayerView.swift
@@ -0,0 +1,26 @@
+//
+//  CALayerView.swift
+//  TeePod
+//
+//  Created by 石井知恵子 on 2020/06/23.
+//  Copyright © 2020 TeePod. All rights reserved.
+//
+
+import SwiftUI
+
+struct CALayerView: UIViewControllerRepresentable {
+    var caLayer:CALayer
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CALayerView>) -> UIViewController {
+        let viewController = UIViewController()
+
+        viewController.view.layer.addSublayer(caLayer)
+        caLayer.frame = viewController.view.layer.frame
+
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<CALayerView>) {
+        caLayer.frame = uiViewController.view.layer.frame
+    }
+}
--- TeePod/modules/Ar/ArView.swift
+++ TeePod/modules/Ar/ArView.swift
@@ -8,17 +8,31 @@
 
 import SwiftUI
 
+
 struct ArView: View {
-  @ObservedObject var presenter: ArPresenter
-  
+    @ObservedObject var presenter: ArPresenter
+    @ObservedObject private var avFoundationVM = AVFoundationVM()
+    
     var body: some View {
-        Text("This is AR page")
-    }
-}
+        VStack {
+            ZStack(alignment: .bottom) {
+                CALayerView(caLayer: avFoundationVM.previewLayer)
+            }.onAppear {
+                self.avFoundationVM.startSession()
+            }.onDisappear {
+                self.avFoundationVM.endSession()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+        }
+    }}
 
 struct ArView_Previews: PreviewProvider {
     static var previews: some View {
-      let presenter = ArPresenter()
-      return ArView(presenter: presenter)
+        let presenter = ArPresenter()
+        return Group{
+            ArView(presenter: presenter).previewDevice(PreviewDevice(rawValue: "iPhone 7 Plus"))
+            ArView(presenter: presenter).previewDevice(PreviewDevice(rawValue: "iPhone 7"))
+        }
     }
 }
